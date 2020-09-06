009e30b2d97a43565e2fc20526d534c0bc812575
Shomari Prince <nyusternie@sdot.io>
2020-09-06 10:24:47 -0400

    Complete first Link integration with `getWalletProviderStatus`.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,8 +7,27 @@
         <hr class="my-1" />
 
         <div class="row">
-            <div class="col">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <div class="col-5 text-right">
+                Badger:
+            </div>
+            <div class="col-7">
+                {{status.badger}}
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-5 text-right">
+                Android:
+            </div>
+            <div class="col-7">
+                {{status.android}}
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-5 text-right">
+                iOS:
+            </div>
+            <div class="col-7">
+                {{status.ios}}
             </div>
         </div>
 
@@ -19,32 +38,16 @@
                     eiusmod imitarentur ex admodum quo laboris labore fabulas voluptate id duis
                     incurreret non eram quorum hic possumus fugiat nostrud ab ita dolore doctrina
                     praetermissum hic cernantur nisi irure appellat velit, id laborum qui ingeniis.
-                    Senserit quem mandaremus, si admodum o occaecat, ipsum arbitror fabulas ita quae
-                    eu fabulas non nisi ad offendit est voluptate an anim in ita esse laboris ubi
-                    fabulas id consequat, legam deserunt est commodo.Occaecat consectetur iis
-                    quibusdam aut aute et arbitror. Te amet senserit fabulas hic sed nescius
-                    efflorescere ne dolor cupidatat a quamquam quo possumus ut eram cupidatat, hic
-                    illum ita enim te se irure quorum quis arbitror, et noster nescius nam ab labore
-                    singulis relinqueret. Te possumus e possumus, eiusmod id iudicem. Iis noster
-                    nescius.
                 </p>
             </div>
 
             <div class="col">
                 <p>
-                    Pariatur magna culpa probant aute, qui labore nisi non cupidatat, laboris veniam
-                    culpa proident minim, cillum laboris ab familiaritatem. Et quis constias do
-                    irure officia deserunt, culpa iudicem sed labore magna, do veniam expetendis
-                    cohaerescant. Sed nescius te mentitum ab ita fore elit sint aliquip, enim
-                    quamquam o arbitrantur, laborum transferrem et aliquip, nam legam quo labore,
-                    quorum expetendis incurreret ne commodo relinqueret et quamquam e an noster
-                    instituendarum.Summis id expetendis id anim in culpa nam arbitror iis fore, do
-                    export deserunt cernantur, cernantur amet veniam incurreret fugiat se tamen et
-                    quo enim singulis ne mentitum duis singulis, ne fugiat ita tamen, officia dolore
-                    sunt quamquam dolor. A export elit summis possumus ut summis admodum id quae
-                    elit, labore deserunt ne efflorescere, mandaremus non fugiat cernantur, id velit
-                    duis iis proident an a duis appellat an export voluptate firmissimum, probant
-                    quorum incididunt mentitum. Te non voluptatibus.
+                    Incididunt ubi aute senserit et do cernantur ea occaecat, tamen cohaerescant
+                    incurreret labore vidisse, summis ab deserunt an velit o incididunt sunt multos
+                    ut cillum id ab fore duis anim voluptate ut tamen iudicem a aliquip, iis
+                    proident consectetur. Est dolore possumus. Do admodum distinguantur, cernantur
+                    se deserunt.
                 </p>
             </div>
         </div>
@@ -52,17 +55,26 @@
 </template>
 
 <script>
+/* Import modules. */
+import bchLink from 'bitcoincom-link'
+
 export default {
     components: {
         // HelloWorld
     },
     data: () => {
         return {
-            //
+            output: null,
+            status: null,
         }
     },
     created: function () {
 
+        /* Request provider statuses. */
+        this.status = bchLink.getWalletProviderStatus()
+        console.log('Provider statuses:', this.status)
+
+        this.output = JSON.stringify(this.status, null, 2)
     },
     mounted: function () {
 
